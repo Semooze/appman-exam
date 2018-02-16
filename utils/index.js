13edd66d027392c094755857ed6a439f42b42b9a
Semooze <cloudtimewater@gmail.com>
2018-02-16 13:55:34 +0700

    Add update student score and remove student score by subject.

--- utils/index.js
+++ utils/index.js
@@ -9,7 +9,32 @@ const _ = require('lodash');
  * @params {Number} scores{key}
  */
 exports.updateStudentScore = (store, { name, scores }) => {
-    // code here
+
+    let newStore = _.cloneDeep(store);
+    let newHash = _.keyBy(store, 'subject');
+
+    for(let subject in scores) {
+        if (!newHash.hasOwnProperty(subject)) {
+            newStore = [].concat(newStore, { subject: subject, students:[{ name: name, score: scores[subject]}]})
+        }
+    }
+
+    newStore.map((item) => {
+
+        let newStudent = true;
+        item.students.map((student) => {
+            if (student.name === name) {
+                student.score = scores[item.subject];
+                newStudent = false;
+            }
+        });
+
+        if (newStudent && scores.hasOwnProperty(item.subject)) {
+            item.students = [].concat(item.students, { name: name, score: scores[item.subject]});
+        }
+    });
+
+    return newStore;
 };
 
 /**
@@ -18,7 +43,14 @@ exports.updateStudentScore = (store, { name, scores }) => {
  * @params {String} subject
  */
 exports.removeStudentScoreBySubject = (store, { name, subject }) => {
-    // code here
+
+    let newStore = _.cloneDeep(store);
+    newStore.map((item) => {
+        if (item.subject === subject) {
+            item.students = item.students.filter((student) => student.name !== name);
+        }
+    });
+    return newStore;
 };
 
 /**
